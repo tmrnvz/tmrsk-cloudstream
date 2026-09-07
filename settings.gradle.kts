e899b030549a60a09f67c87fa8eb7b207c332e80
tmrnvz <t.buradaydi@gmail.com>
2026-09-07 07:36:50 +0300

    Publish tmrsk cleanup migration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,7 @@ rootProject.name = "CloudstreamPlugins"
 // This file sets what projects are included.
 // All new projects should get automatically included unless specified in the "disabled" variable.
 
-val disabled = listOf("ExampleProvider")
+val disabled = listOf("ExampleProvider", "Migration")
 
 File(rootDir, ".").eachDir { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
@@ -19,3 +19,6 @@ include("CanliYayin")
 project(":CanliYayin").projectDir = file("ExampleProvider")
 
 include("Famelack")
+
+include("tmrsk")
+project(":tmrsk").projectDir = file("Migration")
