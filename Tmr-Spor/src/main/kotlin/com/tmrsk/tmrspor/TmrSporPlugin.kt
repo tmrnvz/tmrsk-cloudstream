8694e1a5b7e1ede3336a347b64d75c6ee5a56f0d
tmrnvz <t.buradaydi@gmail.com>
2026-09-08 00:39:31 +0300

    Add independent Tmr-Spor M3U plugin

---
+++ Tmr-Spor/src/main/kotlin/com/tmrsk/tmrspor/TmrSporPlugin.kt
@@ -0,0 +1,12 @@
+package com.tmrsk.tmrspor
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class TmrSporPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(TmrSporProvider())
+    }
+}
