56eb739d41077b5ccec9a3d5bb3bb4d1ca881d24
tmrnvz <t.buradaydi@gmail.com>
2026-09-07 19:34:39 +0300

    Add legal TmrPal educational provider template

---
+++ TmrPalTemplate/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "Yasal yayın siteleri için eğitim amaçlı CloudStream şablonu"
+    authors = listOf("tmrsk")
+    status = 3
+    tvTypes = listOf("Movie", "TvSeries")
+    requiresResources = false
+    language = "tr"
+}
