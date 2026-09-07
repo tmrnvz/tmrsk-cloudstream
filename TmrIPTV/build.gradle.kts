7755e4965e1676eeaddab0c4979de550f6ff9f52
tmrnvz <t.buradaydi@gmail.com>
2026-09-07 20:33:58 +0300

    Add editable tmrIPTV M3U plugin

---
+++ TmrIPTV/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    description = "GitHub üzerindeki kişisel M3U kanal listesini oynatır"
+    authors = listOf("tmrsk")
+    status = 3
+    tvTypes = listOf("Live")
+    requiresResources = false
+    language = "tr"
+}
