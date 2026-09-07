347ed30a5adad4ab3848bfe4b11333ad15e8f413
tmrnvz <t.buradaydi@gmail.com>
2026-09-07 16:59:20 +0300

    Add independent Dunya Radyolari plugin

---
+++ DunyaRadyolari/src/main/kotlin/com/tmrsk/radio/DunyaRadyolariPlugin.kt
@@ -0,0 +1,12 @@
+package com.tmrsk.radio
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class DunyaRadyolariPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(DunyaRadyolariProvider())
+    }
+}
