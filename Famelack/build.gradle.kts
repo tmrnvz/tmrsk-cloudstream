30b6b00479873d91eaec150559a12f442a4f3897
tmrnvz <t.buradaydi@gmail.com>
2026-09-07 07:03:38 +0300

    Add Famelack/build.gradle.kts

---
+++ Famelack/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "Famelack dünya TV, radyo ve canlı kameraları"
+    authors = listOf("tmrsk")
+    status = 1
+    tvTypes = listOf("Live", "TvSeries")
+    requiresResources = false
+    language = "tr"
+    iconUrl = "https://famelack.com/assets/favicons/favicon-512.png"
+}
