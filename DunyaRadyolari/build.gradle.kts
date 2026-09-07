52d760512183ca71591b51e4c40db2c7475cd41e
tmrnvz <t.buradaydi@gmail.com>
2026-09-07 16:58:53 +0300

    Add independent Dunya Radyolari plugin

---
+++ DunyaRadyolari/build.gradle.kts
@@ -0,0 +1,11 @@
+version = 1
+
+cloudstream {
+    description = "Dünya radyoları; Türkiye doğrudan, diğer ülkeler ayrı listelenir"
+    authors = listOf("tmrsk")
+    status = 1
+    tvTypes = listOf("Live")
+    requiresResources = false
+    language = "tr"
+    iconUrl = "https://raw.githubusercontent.com/tmrnvz/tmrsk-cloudstream/main/images/SynqBrand-Radio.png"
+}
