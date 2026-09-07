6e1de0f45f0874d730e9a26adbc92785874a5368
tmrnvz <t.buradaydi@gmail.com>
2026-09-08 01:11:18 +0300

    Tmr-Dizi: büyük M3U dosyalarını destekle

--- Tmr-Dizi/src/main/kotlin/com/tmrsk/tmrdizi/TmrDiziProvider.kt
+++ Tmr-Dizi/src/main/kotlin/com/tmrsk/tmrdizi/TmrDiziProvider.kt
@@ -21,7 +21,7 @@ class TmrDiziProvider : MainAPI() {
 
     private suspend fun channels(): List<Channel> {
         cachedChannels?.let { return it }
-        return parseM3u(app.get(mainUrl).text).also { cachedChannels = it }
+        return parseM3u(app.get(mainUrl).textLarge).also { cachedChannels = it }
     }
 
     private suspend fun series(): List<Series> = channels()
