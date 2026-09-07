e25030b4657df9580544d33b87fc29091f51e667
tmrnvz <t.buradaydi@gmail.com>
2026-09-07 07:36:15 +0300

    Add legacy tmrsk cleanup package

---
+++ Migration/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 2
+
+cloudstream {
+    language = "tr"
+    description = "Eski birleşik tmrsk paketini temizler. Güncel eklentileri ayrı kurun."
+    authors = listOf("tmrsk")
+    status = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://raw.githubusercontent.com/tmrnvz/tmrsk-cloudstream/main/icon.png"
+}
